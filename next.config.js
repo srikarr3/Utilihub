556b69e763fecf91569662f06d1e0d5917011e10
Srikar <114099501+srikarr3@users.noreply.github.com>
2025-04-10 21:14:04 +0530

    added commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Required for GitHub Pages
-  basePath: process.env.NODE_ENV === 'production' ? '/Utilhub' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Utilhub/' : '',
-  
   // Enable static exports
   images: {
     unoptimized: true,
